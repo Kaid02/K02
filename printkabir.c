db41ca5319099eb76a8e72baf7225089a7c77b54
Kaid02 <kabirS317@gmail.com>
2016-10-17 11:11:55 +0545

    Add files via upload

---
+++ printkabir.c
@@ -0,0 +1,17 @@
+/*    
+Student name : Kabir D Shrestha
+Subject : Programming Fundamental
+Lab No. : 07
+Program : Print your Name
+Date : 2016-10-17
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+	
+	int main(){
+		printf("My name is Kabir D Shrestha");
+		return(0);
+		//return 0;
+	}
+	
